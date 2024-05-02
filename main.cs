38d21f5c22e96c3a7574e5ac0f5a48cb4a07b2d3
Notsickfox <88145761+scaranaraa@users.noreply.github.com>
2024-05-02 11:31:33 +0530

    initial commit

---
+++ main.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class main : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	public void _on_quit_pressed(){
+		GetTree().Quit();
+	}
+
+	public void _on_play_pressed(){
+		GetTree().ChangeSceneToFile("res://world.tscn");
+	}	
+}
+
